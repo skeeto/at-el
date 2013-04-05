ecb4b7a5e683b2c81fcd385ec6feeb7b7fefbc9d
Christopher Wellons <mosquitopsu@gmail.com>
2013-04-04 21:19:18 -0400

    Start building a library. Add @queue.

---
+++ lib/@queue-tests.el
@@ -0,0 +1,21 @@
+(require 'ert)
+(defalias 'deftest 'ert-deftest)
+
+(deftest @queue-test ()
+  (let ((q (@extend @queue)))
+    (@! q :enqueue 0)
+    (@! q :enqueue 1)
+    (@! q :dequeue)
+    (@! q :enqueue 2)
+    (@! q :enqueue 3)
+    (should
+     (equal '(1 2 3)
+            (loop until (@! q :emptyp) collect (@! q :dequeue)))))
+  (let ((q (@extend @queue)))
+    (@! q :enqueue 'b)
+    (should (eq 'b (@! q :peek)))
+    (@! q :enqueue 'c)
+    (@! q :prepend 'a)
+    (should
+     (equal '(a b c)
+            (@! q :to-list)))))
---
+++ lib/@queue.el
@@ -0,0 +1,37 @@
+;;; @queue.el --- queue prototype written in @
+
+(require '@)
+
+(defvar @queue (@extend :head nil :tail nil))
+
+(def@ @queue :emptyp ()
+  "Return t if this queue is empty."
+  (null @:head))
+
+(def@ @queue :enqueue (element)
+  "Add ELEMENT to the end of this queue. Return this queue."
+  (prog1 @@
+      (if (@:emptyp)
+          (setf @:head (list element)
+                @:tail @:head)
+        (setf (cdr @:tail) (list element)
+              @:tail (cdr @:tail)))))
+
+(def@ @queue :dequeue ()
+  "Remove and return element at the front of this queue."
+  (pop @:head))
+
+(def@ @queue :prepend (element)
+  "Prepend ELEMENT to the front of this queue. Return this queue."
+  (prog1 @@
+      (push element @:head)))
+
+(def@ @queue :peek ()
+  "Return the element at the front of the queue without returning it."
+  (car @:head))
+
+(def@ @queue :to-list ()
+  "Return this entire queue as a list."
+  (copy-list @:head))
+
+;;; @queue.el ends here
