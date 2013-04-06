ebd909f49e23f3b47d673f1d98a776d72dc9f9e1
Christopher Wellons <mosquitopsu@gmail.com>
2013-04-06 18:33:52 -0400

    Fix @heap-tests.el typos.

--- lib/@heap-tests.el
+++ lib/@heap-tests.el
@@ -9,7 +9,7 @@
     (@! h :add '(c . 3))
     (@! h :add '(a . 1))
     (should (eq 'a (car (@! h :peek))))
-    (should (equal '(a b c d) (map #'car (@! h :to-list))))
+    (should (equal '(a b c d) (mapcar #'car (@! h :to-list))))
     (should
      (equal '(a b c d)
-            (loop until (@! h :emptyp) collect (car (@! h :next))))))))
+            (loop until (@! h :emptyp) collect (car (@! h :next)))))))
