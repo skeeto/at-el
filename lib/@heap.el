57c08e39dbc56190c1927cc4943d04d4bf4456e4
Christopher Wellons <mosquitopsu@gmail.com>
2013-04-06 13:06:51 -0400

    Fix up @heap to work with a key and a compare function.

--- lib/@heap.el
+++ lib/@heap.el
@@ -2,11 +2,13 @@
 
 (require '@)
 
-(defvar @heap (@extend :vector nil)
-  "A min-heap prototype.")
+(defvar @heap (@extend :vector nil :heap-key #'identity :heap-compare #'<)
+  "A priority heap prototype with extendable key and compare functions.")
 
-(def@ @heap :init ()
-  (setf @:vector (@! @vector :new)))
+(def@ @heap :init (&optional key compare)
+  (setf @:vector (@! @vector :new))
+  (when key (setf @:heap-key key))
+  (when compare (setf @:heap-compare key)))
 
 (def@ @heap :emptyp ()
   "Return t if this heap is empty."
@@ -14,33 +16,41 @@
 
 (def@ @heap :peek ()
   "Return the next output element of the heap without removing it."
-  (car (@ @:vector 0)))
+  (@ @:vector 0))
 
-(def@ @heap :add (element priority)
+(def@ @heap :add (element)
   "Add ELEMENT to this heap with PRIORITY priority. Return this heap."
   (prog1 @@
     (let* ((i (@! @:vector :size))
-           (parent (floor (1- i) 2)))
-      (@! @:vector :push (cons element priority))
-      (flet ((priority (p) (cdr (@ @:vector p))))
-        (while (and (>= parent 0) (< priority (priority parent)))
+           (parent (floor (1- i) 2))
+           (key @:heap-key)
+           (compare @:heap-compare))
+      (@! @:vector :push element)
+      (flet ((compare (a b)
+              (funcall compare (funcall key (@ @:vector a))
+                               (funcall key (@ @:vector b)))))
+        (while (and (>= parent 0) (compare i parent))
           (@! @:vector :swap i parent)
           (setf i parent
                 parent (floor (1- parent) 2)))))))
 
 (def@ @heap :next ()
   "Remove and return the next element in the heap."
-  (prog1 (car (@ @:vector 0))
+  (prog1 (@ @:vector 0)
     (let* ((replace (@! @:vector :pop))
-           (priority (cdr replace)))
+           (priority (funcall @:heap-key replace)))
       (unless (@! @:vector :emptyp)
         (setf (@ @:vector 0) replace)
-        (loop for i = 0 then largest
+        (loop with compare = @:heap-compare
+              with key = @:heap-key
+              for i = 0 then largest
               for a = (+ 1 (* i 2)) and b = (+ 2 (* i 2))
-              for na = (@ @:vector a) and nb = (@ @:vector b)
-              for largest = (cond ((and na (< (cdr na) priority)) a)
-                                  ((and nb (< (cdr nb) priority)) b)
-                                  (i))
+              for na = (@ @:vector a)
+              and nb = (@ @:vector b)
+              for largest =
+              (cond ((and na (funcall compare (funcall key na) priority)) a)
+                    ((and nb (funcall compare (funcall key nb) priority)) b)
+                    (i))
               while (not (= largest i))
               do (@! @:vector :swap i largest))))))
 
